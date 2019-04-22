9252f488e2fc21112df6e31aea1899cd90a095dd
Roei5591 <34808057+Roei5591@users.noreply.github.com>
2019-04-22 16:25:29 +0300

    Sohere intersect func

--- src/edu/cg/scene/objects/Sphere.java
+++ src/edu/cg/scene/objects/Sphere.java
@@ -4,6 +4,7 @@ import edu.cg.UnimplementedMethodException;
 import edu.cg.algebra.Hit;
 import edu.cg.algebra.Point;
 import edu.cg.algebra.Ray;
+import edu.cg.algebra.Vec;
 
 public class Sphere extends Shape {
 	private Point center;
@@ -36,7 +37,64 @@ public class Sphere extends Shape {
 
 	@Override
 	public Hit intersect(Ray ray) {
-		// TODO: implement this method.
-		throw new UnimplementedMethodException("intersect(Ray)");
+		// TODO: implement this method. done
+		Point sourcePoint = ray.source(); //p
+		Vec directionVec = ray.direction(); //d
+		Vec projectFromCenter = sourcePoint.sub(center);//
+
+		//sourcePoint + (directionVec.projectFromCenter) * directionVec
+		//http://www.lighthouse3d.com/tutorials/maths/ray-sphere-intersection/
+		Point pc = sourcePoint.add(directionVec.mult(directionVec.dot(projectFromCenter)));
+
+		double disOfRayFromCenter = pc.dist(center);// |c - pc|
+		double dist = Math.sqrt(Math.pow(radius,2) - Math.pow(disOfRayFromCenter,2));
+
+		Hit hit = null;
+		double t;
+		Point intersection;
+
+		Vec sourceToCenterVec = sourcePoint.sub(center);  // this is the vector from p to c
+
+		if ( (sourceToCenterVec.dot(directionVec)) < 0) // when the sphere is behind the origin p
+		{
+			if (sourceToCenterVec.length() == radius) // when source point is on the sphere
+			{
+				intersection = sourcePoint;
+				Vec normalToSphere = intersection.sub(center).normalize();
+				hit = new Hit(0,normalToSphere);
+			}
+			else if ( sourceToCenterVec.length() < radius) //  when source point is inside the sphere
+			{
+				t = dist - pc.dist(sourcePoint);
+				// intersetionPoint = sourcePoint + t * directionVe
+				intersection = sourcePoint.add( directionVec.mult(t));
+
+				Vec normalToSphere = intersection.sub(center).normalize();
+				hit = new Hit(t,normalToSphere);
+			}
+		}
+		else // center of sphere projects on the ray
+		{
+			if (disOfRayFromCenter <= radius)
+			{
+
+				if (sourceToCenterVec.length() > radius) // source point is outside the sphere
+				{
+					t = sourcePoint.dist(pc) - dist;
+				}
+				else // source point is inside or on the sphere
+				{
+					t = sourcePoint.dist(pc) + dist;
+				}
+
+				// intersetionPoint = sourcePoint + t * directionVe
+				intersection = sourcePoint.add( directionVec.mult(t));
+				Vec normalToSphere = intersection.sub(center).normalize();
+				hit = new Hit(t,normalToSphere);
+			}
+		}
+
+		return hit;
+
 	}
 }

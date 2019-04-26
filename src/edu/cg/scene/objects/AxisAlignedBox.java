a6c1f30ae74d9011565cd4f4c24de8e012120d87
Roei5591 <34808057+Roei5591@users.noreply.github.com>
2019-04-26 18:14:56 +0300

    Update AxisAlignedBox.java

--- src/edu/cg/scene/objects/AxisAlignedBox.java
+++ src/edu/cg/scene/objects/AxisAlignedBox.java
@@ -4,6 +4,10 @@ import edu.cg.UnimplementedMethodException;
 import edu.cg.algebra.Hit;
 import edu.cg.algebra.Point;
 import edu.cg.algebra.Ray;
+import edu.cg.algebra.Vec;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 public class AxisAlignedBox extends Shape {
 	private Point minPoint;
@@ -34,9 +38,9 @@ public class AxisAlignedBox extends Shape {
 	 * This methods fixes the boundary points minPoint and maxPoint so that the values are consistent.
 	 */
 	private void fixBoundryPoints() {
-		double min_x = Math.min(minPoint.x, maxPoint.x), max_x = Math.max(minPoint.x, maxPoint.x),
-				min_y = Math.min(minPoint.y, maxPoint.y), max_y = Math.max(minPoint.y, maxPoint.y),
-				min_z = Math.min(minPoint.z, maxPoint.z), max_z = Math.max(minPoint.z, maxPoint.z);
+		double min_x = min(minPoint.x, maxPoint.x), max_x = max(minPoint.x, maxPoint.x),
+				min_y = min(minPoint.y, maxPoint.y), max_y = max(minPoint.y, maxPoint.y),
+				min_z = min(minPoint.z, maxPoint.z), max_z = max(minPoint.z, maxPoint.z);
 		minPoint = new Point(min_x, min_y, min_z);
 		maxPoint = new Point(max_x, max_y, max_z);
 	}
@@ -61,11 +65,81 @@ public class AxisAlignedBox extends Shape {
 	}
 
 	@Override
-	public Hit intersect(Ray ray) {
-		// TODO You need to implement this method. 
-		// See documentation in Intersectable.java base class. 
-		throw new UnimplementedMethodException("intersect");
+	public Hit intersect(Ray ray)
+	{
+		// TODO You need to implement thismethod.
+		double tmin = Double.NEGATIVE_INFINITY;
+		double tmax = Double.POSITIVE_INFINITY;
+
+		if (ray.direction().x != 0.0) {
+			double tx1 = (minPoint.x - ray.source().x)/ ray.direction().x;
+			double tx2 = (maxPoint.x - ray.source().x)/ ray.direction().x;
+			tmin = max(tmin, min(tx1, tx2));
+			tmax = min(tmax, max(tx1, tx2));
+		}
+
+		if (ray.direction().y != 0.0) {
+			double ty1 = (minPoint.y - ray.source().y)/ ray.direction().y;
+			double ty2 = (maxPoint.y - ray.source().y)/ ray.direction().y;
+
+			tmin = max(tmin, min(ty1, ty2));
+			tmax = min(tmax, max(ty1, ty2));
+		}
+
+		if (ray.direction().z != 0.0) {
+			double tz1 = (minPoint.z - ray.source().z)/ ray.direction().z;
+			double tz2 = (maxPoint.z - ray.source().z)/ ray.direction().z;
+
+			tmin = max(tmin, min(tz1, tz2));
+			tmax = min(tmax, max(tz1, tz2));
+		}
+
+		if (tmax >= tmin)
+		{
+			Point hitPoint = ray.source().add(tmin , ray.direction());
+			return new Hit(tmin , findNormalOnIntersectPoint(hitPoint));
+		}
+
+			return null;
 	}
+
+	public Vec findNormalOnIntersectPoint(Point IntersectPoint)
+	{
+		Vec normalOnIntersectPoint = null;
+
+		if(IntersectPoint.x == minPoint.x)
+		{
+			normalOnIntersectPoint = new Vec(-1 ,0 ,0);
+		}
+		else if(IntersectPoint.x == maxPoint.x)
+		{
+			normalOnIntersectPoint = new Vec(1 ,0 ,0);
+		}
+		else if(IntersectPoint.y == minPoint.y)
+		{
+			normalOnIntersectPoint = new Vec( 0,-1 ,0);
+		}
+		else if(IntersectPoint.y == maxPoint.y)
+		{
+			normalOnIntersectPoint = new Vec(0 ,1 ,0);
+		}
+		else if(IntersectPoint.z == minPoint.z)
+		{
+			normalOnIntersectPoint = new Vec(0 ,0 ,-1);
+		}
+		else if(IntersectPoint.z == maxPoint.z)
+		{
+			normalOnIntersectPoint = new Vec(0 ,0 ,1);
+		}
+		else
+		{
+			System.out.println("Error in  AxisAlignedBox");
+		}
+
+		return normalOnIntersectPoint;
+	}
+
+
 
 	
 }

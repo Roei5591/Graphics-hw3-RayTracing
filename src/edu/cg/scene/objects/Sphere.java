f679af04756e5ac8590e117902e925494623df31
Ofek Dadush <38763494+ofekdadu@users.noreply.github.com>
2019-04-29 12:24:43 +0300

    working sphere

--- src/edu/cg/scene/objects/Sphere.java
+++ src/edu/cg/scene/objects/Sphere.java
@@ -1,10 +1,7 @@
 package edu.cg.scene.objects;
 
 import edu.cg.UnimplementedMethodException;
-import edu.cg.algebra.Hit;
-import edu.cg.algebra.Point;
-import edu.cg.algebra.Ray;
-import edu.cg.algebra.Vec;
+import edu.cg.algebra.*;
 
 public class Sphere extends Shape {
 	private Point center;
@@ -35,83 +32,37 @@ public class Sphere extends Shape {
 		return this;
 	}
 
-
-	public Hit intersect1(Ray ray)
-	{
-		return null;
-	}
-
-
-
-
-
-	@Override
-	public Hit intersect(Ray ray) {
-		// TODO: implement this method. done
-		Point sourcePoint = ray.source(); //p
-		Vec directionVec = ray.direction(); //d
-		//Vec projectFromCenter = sourcePoint.sub(center);// p to c
-		Vec projectFromCenter = center.sub(sourcePoint);
-
-		//sourcePoint + (directionVec.projectFromCenter) * directionVec
-		//http://www.lighthouse3d.com/tutorials/maths/ray-sphere-intersection/
-		Point pc = sourcePoint.add(directionVec.mult(directionVec.dot(projectFromCenter)));
-
-		double disOfRayFromCenter = pc.dist(center);// |c - pc|
-		double dist = Math.sqrt(Math.pow(radius,2) - Math.pow(disOfRayFromCenter,2));
-
-		Hit hit = null;
-		double t;
-		Point intersection;
-
-		//Vec projectFromCenter = sourcePoint.sub(center);  // this is the vector from p to c
-
-		if ( (projectFromCenter.dot(directionVec)) < 0) // when the sphere is behind the origin p
-		{
-			if (projectFromCenter.length() == radius) // when source point is on the sphere
-			{
-				intersection = sourcePoint;
-				Vec normalToSphere = intersection.sub(center).normalize();
-				hit = new Hit(0,normalToSphere);
-			}
-			else if ( projectFromCenter.length() < radius) //  when source point is inside the sphere
-			{
-				t = dist - pc.dist(sourcePoint);
-				// intersetionPoint = sourcePoint + t * directionVe
-				intersection = sourcePoint.add( directionVec.mult(t));
-
-				Vec normalToSphere = intersection.sub(center).normalize();
-				hit = new Hit(t,normalToSphere);
-				hit.setWithin();
-			}
-		}
-		else // center of sphere projects on the ray
-		{
-			if (disOfRayFromCenter <= radius)
-			{
-				boolean isWithin;
-
-				if (projectFromCenter.length() > radius) // source point is outside the sphere
-				{
-					t = sourcePoint.dist(pc) - dist;
-					isWithin = false;
-				}
-				else // source point is inside or on the sphere
-				{
-					t = sourcePoint.dist(pc) + dist;
-					isWithin = true;
-				}
-
-				// intersetionPoint = sourcePoint + t * directionVe
-				intersection = sourcePoint.add( directionVec.mult(t));
-
-				Vec normalToSphere = intersection.sub(center).normalize();
-				hit = new Hit(t,normalToSphere);
-				hit.setIsWithin(isWithin);
-			}
-		}
-
-		return hit;
-
-	}
+    @Override
+    public Hit intersect(Ray ray) {
+        // TODO: implement this method.
+        double a = 1;
+        double b = ray.direction().mult(2.0).dot(ray.source().sub(center));
+        double c = ray.source().distSqr(center) - Math.pow(radius, 2);
+        double delta = Math.pow(b, 2) - (4 * a * c) ;
+        if(delta < 0) return null;
+        if(delta == 0){
+            double t = -b / ( 2 * a);
+            if(t < Ops.epsilon || t > Ops.infinity){
+                return null;
+            }
+            Vec norm = ray.add(t).sub(center).normalize();
+            return new Hit(t, norm);
+        }
+        //delta > 0
+        double t1 = (-b + Math.sqrt(delta)) / ( 2 * a);
+        double t2 = (-b - Math.sqrt(delta)) / ( 2 * a);
+        if(t1 < Ops.epsilon) return null;// both are behind
+        double t = t2;
+        boolean isWithin = false;
+        Vec norm = ray.add(t).sub(center).normalize();
+        if(t2 < Ops.epsilon) {// we are inside
+            t = t1;
+            isWithin = true;
+            norm = ray.add(t).sub(center).normalize().neg();
+        }
+        if(t > Ops.infinity){
+            return null;
+        }
+        return new Hit(t, norm).setIsWithin(isWithin);
+    }
 }
